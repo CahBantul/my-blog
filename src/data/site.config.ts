06edbd42f091054f48ec78b212f746716477adca
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-04-07 22:24:34 +0700

    adding post

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Nozami', // Site author
+	title: 'Nozami', // Site title.
+	description: 'Unlock programming expertise with our concise, insightful blog posts.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
