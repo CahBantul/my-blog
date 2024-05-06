295ff4c4956e276e74d5444f9a4762406ba2a372
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-05-06 13:29:00 +0700

    feat: adding laravel messenger#4

--- scripts/replaceAssetPaths.js
+++ scripts/replaceAssetPaths.js
@@ -1,31 +1,66 @@
 // all .md files in this directory will be processed
 // specify from the root of your project
-const postsDirectory = "/src/content/blog/";
+const postsDirectory = '/src/content/blog/'
+import { exec } from 'child_process'
 
 // Find and replace strings
-const find = "/src/assets/images";
-let replace = "../../../src/assets/images"; // update this path based on where files are located
+const findFirst = '../../../src/assets/images' // update this path based on where files are located
+let replaceFirst = '/src/assets/images'
 
-replace = replace.replaceAll(".", "\\.");
+replaceFirst = replaceFirst.replaceAll('.', '\\.')
 // Special characters (https://en.wikipedia.org/wiki/Regular_expression#POSIX_basic_and_extended) need to be escaped
 
-import { exec } from "child_process";
 // execute bash command
 exec(
-  `find ${process.cwd()}${postsDirectory} -type f -name '*.mdx' -print0 | xargs -0 sed -i -e 's:${find}:${replace}:g'`,
-  // GNU sed that runs on Linux but not on mac
-  (error, stdout, stderr) => {
-    // error handling
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    // success
-    console.log(stdout);
-    console.log("🖼️ Successfully replaced asset paths");
-  }
-);
+	`find ${process.cwd()}${postsDirectory} -type f -name '*.mdx' -print0 | xargs -0 sed -i -e 's:${findFirst}:${replaceFirst}:g'`,
+	// GNU sed that runs on Linux but not on mac
+	(error, stdout, stderr) => {
+		// error handling
+		if (error) {
+			console.log(`error: ${error.message}`)
+			return
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`)
+			return
+		}
+		// success
+		console.log(stdout)
+		console.log('🖼️ Successfully replaced asset paths pertama')
+	}
+)
+
+// Fungsi untuk melakukan sesuatu setelah 2 detik
+function doSomething() {
+	console.log('Waktu 2 detik telah berlalu. Tindakan dilakukan.')
+	// Tempatkan tindakan yang ingin Anda lakukan di sini
+	// Find and replace strings
+	const find = '/src/assets/images'
+	let replace = '../../../src/assets/images' // update this path based on where files are located
+
+	replace = replace.replaceAll('.', '\\.')
+	// Special characters (https://en.wikipedia.org/wiki/Regular_expression#POSIX_basic_and_extended) need to be escaped
+
+	// execute bash command
+	exec(
+		`find ${process.cwd()}${postsDirectory} -type f -name '*.mdx' -print0 | xargs -0 sed -i -e 's:${find}:${replace}:g'`,
+		// GNU sed that runs on Linux but not on mac
+		(error, stdout, stderr) => {
+			// error handling
+			if (error) {
+				console.log(`error: ${error.message}`)
+				return
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`)
+				return
+			}
+			// success
+			console.log(stdout)
+			console.log('🖼️ Successfully replaced asset paths kedua')
+		}
+	)
+}
+
+// Atur timeout selama 2 detik
+setTimeout(doSomething, 2000)
