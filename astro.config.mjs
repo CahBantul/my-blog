7fac65bfe2ef072b73e6a6e01fd9d467736e480c
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-05-12 12:33:32 +0700

    feat: laravel reverb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
-import { externalLink } from 'src/externalLink.ts'
+import { externalLink } from './src/externalLink.ts'
 
 // https://astro.build/config
 export default defineConfig({
