b7219cb51cbf2e65aaa2dddd9227009c47e0c748
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-06-30 12:45:14 +0700

    adding laravel filament post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
-import { externalLink } from './src/externalLink.ts'
 import { rehypePrettyCode } from 'rehype-pretty-code'
-// import moonlightTheme from './public/theme/moonlight-ii.json'
 import { transformerCopyButton } from '@rehype-pretty/transformers'
-import { transformerNotationDiff } from '@shikijs/transformers'
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,13 +17,11 @@ export default defineConfig({
 			[
 				rehypePrettyCode,
 				{
-					// theme: moonlightTheme,
 					transformers: [
 						transformerCopyButton({
 							visibility: 'hover',
 							feedbackDuration: 2_500
 						})
-						// transformerNotationDiff()
 					]
 				}
 			]
